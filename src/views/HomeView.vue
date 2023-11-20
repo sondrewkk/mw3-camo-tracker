87558ed77a13989df2aae4e5ab0da6034216739f
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2023-11-21 00:26:11 +0100

    Added some padding on the homescreen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,8 @@
   <div class="hero min-h-screen">
     <div class="hero-content text-center">
       <div class="max-w-md">
-        <h1 class="text-5xl font-bold">CoD MWIII Camo Tracker</h1>
-        <p class="py-6 text-2xl">
+        <h1 class="text-4xl font-bold">CoD MWIII Camo Tracker</h1>
+        <p class="py-10 text-lg">
           Manually track your camo progress in Modern Warfare III. No login! 
           Progress is stored
           locally on your device.
