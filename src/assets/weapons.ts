3353ce046474ab2c1802458396b1ac163a28880c
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2023-11-22 00:59:04 +0100

    Added MCW to weapons

--- src/assets/weapons.ts
+++ src/assets/weapons.ts
@@ -81,7 +81,7 @@ export const allWeapons: Weapon[] = [
         type: 'BASE',
         gameMode: 'MULTIPLAYER',
         challenge: 'Get 50 kills while ADS',
-        requirement: 'Weapon level 13'
+        requirement: 'Weapon level 11'
       },
       {
         name: 'Germ Factory',
@@ -89,15 +89,77 @@ export const allWeapons: Weapon[] = [
         type: 'BASE',
         gameMode: 'MULTIPLAYER',
         challenge: 'Get 15 headshots kills',
-        requirement: 'Weapon level 20'
+        requirement: 'Weapon level 17'
       },
       {
         name: 'Purple',
+        set: 'SOLID',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 25 kills while in tactical stance',
+        requirement: 'Weapon level 26'
+      },
+      {
+        name: 'Gilded',
+        set: 'DIGITAL',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 3 operator kills with 1 magazine 10 times',
+        requirement: 'Complete all base camo challenges'
+      },
+      {
+        name: 'Forged',
+        set: 'DIGITAL',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills while moving in tac stance',
+        requirement: 'Complete 6 gilded camo challenges with assult rifles'
+      },
+      {
+        name: 'Priceless',
+        set: 'DIGITAL',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 15 headshots kills with a magnification scope',
+        requirement: 'Complete 36 forged camo challenges'
+      }
+    ]
+  },
+  {
+    name: 'MCW',
+    category: 'Assault rifle',
+    camofluages: [
+      {
+        name: 'Razor Feather',
+        set: 'DIGITAL',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 50 kills',
+        requirement: 'Weapon level 2'
+      },
+      {
+        name: 'Skullfragration',
+        set: 'DIGITAL',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 50 kills while ADS',
+        requirement: 'Weapon level 12'
+      },
+      {
+        name: 'Abyssal Fox',
+        set: 'DIGITAL',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 15 headshots kills',
+        requirement: 'Weapon level 19'
+      },
+      {
+        name: 'Menelaus Blue',
         set: 'DIGITAL',
         type: 'BASE',
         gameMode: 'MULTIPLAYER',
         challenge: 'Get 25 kills while in tactical stance',
-        requirement: 'Weapon level 30'
+        requirement: 'Weapon level 28'
       },
       {
         name: 'Gilded',
