03de81603853ec7e56bc7e324c8f2298ecc3d0cb
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2023-11-21 21:16:49 +0100

    Warning progress reset

--- src/views/TrackerView.vue
+++ src/views/TrackerView.vue
@@ -6,6 +6,7 @@ import { computed } from 'vue'
 import ProgressCard from '@/components/ProgressCard.vue'
 import TrackerFilter from '@/components/TrackerFilter.vue'
 import type { WeaponCategory } from '@/composeables/weapon'
+import { ExclamationTriangleIcon } from '@heroicons/vue/24/solid'
 
 const weaponsProgresStore = useWeaponProgressStore()
 const { weaponProgress } = storeToRefs(weaponsProgresStore)
@@ -32,6 +33,10 @@ const groupedProgress = computed(() => {
   <div class="min-h-screen">
     <TrackerFilter />
 
+    <div role="alert" class="alert alert-warning mt-12">
+      <ExclamationTriangleIcon class="h-12 w-12" />
+      <span class="text-lg font-semibold">This is not a final product. Changes may be made that require a complete reset of your progress.</span>
+    </div>
     <div class="flex flex-col px-2 pt-8">
       <ul v-for="(weapons, category) in groupedProgress" :key="category">
         <h2 class="text-3xl font-bold py-8">{{ category }}s</h2>
