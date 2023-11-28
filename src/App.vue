21d4646ba596e277b0aab506ee48385d1e20c00f
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2023-11-29 00:25:43 +0100

    Changed text base color

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import NavigationBar from '@/components/NavigationBar.vue'
 </script>
 
 <template>
-  <div class="w-full bg-base-300 min-h-screen">
+  <div class="w-full bg-base-300 text-base-content min-h-screen">
     <div class="container mx-auto max-w-8xl">
       <NavigationBar />
       <div class="px-4 py-4">
