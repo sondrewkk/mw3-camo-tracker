0b13d89d7ad70c21737e7eaee9ccdefa4af9d3b7
Sondre William Kaldhussæter-Knutsen <knutsensondre@gmail.com>
2023-11-30 01:17:37 +0100

    Added all assultrifles and smgs

--- src/assets/weapons.ts
+++ src/assets/weapons.ts
@@ -126,6 +126,68 @@ export const allWeapons: Weapon[] = [
     ]
   },
   {
+    name: 'HOLGER 556',
+    category: 'Assault rifle',
+    camofluages: [
+      {
+        name: 'Fractal Dirt',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 50 kills',
+        requirement: 'Weapon level 2'
+      },
+      {
+        name: 'Topo Glitch',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 50 kills while ADS',
+        requirement: 'Weapon level 10',
+      },
+      {
+        name: 'Palette Rosemary',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 15 headshots kills',
+        requirement: 'Weapon level 16'
+      },
+      {
+        name: 'Magma Heat',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 25 kills while in tactical stance',
+        requirement: 'Weapon level 24'
+      },
+      {
+        name: 'Gilded',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 3 operator kills with 1 magazine 10 times',
+        requirement: 'Complete all base camo challenges'
+      },
+      {
+        name: 'Forged',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 25 kills with an underbarrel attachment',
+        requirement: 'Complete 6 gilded camo challenges with assult rifles'
+      },
+      {
+        name: 'Priceless',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 15 longshot kills while mounted',
+        requirement: 'Complete 36 forged camo challenges'
+      }
+    ]
+  },
+  {
     name: 'MCW',
     category: 'Assault rifle',
     camofluages: [
@@ -175,6 +237,130 @@ export const allWeapons: Weapon[] = [
         type: 'COMPLETIONIST',
         gameMode: 'MULTIPLAYER',
         challenge: 'Get 10 kills while moving in tac stance',
+        requirement: 'Complete 6 gilded camo challenges with assult rifles'
+      },
+      {
+        name: 'Priceless',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 15 headshots kills with a magnification scope',
+        requirement: 'Complete 36 forged camo challenges'
+      }
+    ]
+  },
+  {
+    name: 'DG-58',
+    category: 'Assault rifle',
+    camofluages: [
+      {
+        name: 'Heat Map',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 50 kills',
+        requirement: 'Weapon level 2'
+      },
+      {
+        name: 'Creeplines',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 50 kills while ADS',
+        requirement: 'Weapon level 11'
+      },
+      {
+        name: 'Twilight Glitch',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 15 headshots kills',
+        requirement: 'Weapon level 17'
+      },
+      {
+        name: 'Oakleaf',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 25 kills while in tactical stance',
+        requirement: 'Weapon level 25'
+      },
+      {
+        name: 'Gilded',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 3 operator kills with 1 magazine 10 times',
+        requirement: 'Complete all base camo challenges'
+      },
+      {
+        name: 'Forged',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills while ADS and strafing',
+        requirement: 'Complete 6 gilded camo challenges with assult rifles'
+      },
+      {
+        name: 'Priceless',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 burst kills',
+        requirement: 'Complete 36 forged camo challenges'
+      }
+    ]
+  },
+  {
+    name: 'FR 5.56',
+    category: 'Assault rifle',
+    camofluages: [
+      {
+        name: 'Get Stony',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 50 kills',
+        requirement: 'Weapon level 2'
+      },
+      {
+        name: 'Digital Bloodbath',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 50 kills while ADS',
+        requirement: 'Weapon level 8'
+      },
+      {
+        name: 'Nightshade',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 15 headshots kills',
+        requirement: 'Weapon level 13'
+      },
+      {
+        name: 'Tiger Blood',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 25 kills while in tactical stance',
+        requirement: 'Weapon level 19'
+      },
+      {
+        name: 'Gilded',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 3 operator kills with 1 magazine 10 times',
+        requirement: 'Complete all base camo challenges'
+      },
+      {
+        name: 'Forged',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 20 operator 1 burst kills',
         requirement: 'Complete 6 gilded camo challenges with assult rifles'
       },
       {
@@ -245,6 +431,316 @@ export const allWeapons: Weapon[] = [
         type: 'COMPLETIONIST',
         gameMode: 'MULTIPLAYER',
         challenge: 'Get 5 kills without dying 10 times',
+        requirement: 'Complete 36 forged camo challenges'
+      }
+    ]
+  },
+  {
+    name: 'WSP SWARM',
+    category: 'Submachine gun',
+    camofluages: [
+      {
+        name: 'Phantasmal',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 50 kills',
+        requirement: 'Weapon level 2'
+      },
+      {
+        name: 'Granite Pass',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 25 hipfire kills',
+        requirement: 'Weapon level 11'
+      },
+      {
+        name: 'Red',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills while croucing or sliding',
+        requirement: 'Weapon level 18'
+      },
+      {
+        name: 'Fritillary',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 15 kills on operators affectred by your tactical',
+        requirement: 'Weapon level 27'
+      },
+      {
+        name: 'Gilded',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills without the enemy damaging you',
+        requirement: 'Complete all base camo challenges'
+      },
+      {
+        name: 'Forged',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 hipfire kills while an enemy is affected by your tactical',
+        requirement: 'Complete 6 gilded camo challenges with assult rifles'
+      },
+      {
+        name: 'Priceless',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 operator double kills while in tac stance',
+        requirement: 'Complete 36 forged camo challenges'
+      }
+    ]
+  },
+  {
+    name: 'AMR9',
+    category: 'Submachine gun',
+    camofluages: [
+      {
+        name: 'Dart',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 50 kills',
+        requirement: 'Weapon level 2'
+      },
+      {
+        name: 'Amaranth Sands',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 25 hipfire kills',
+        requirement: 'Weapon level 12'
+      },
+      {
+        name: 'Microscopic',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills while croucing or sliding',
+        requirement: 'Weapon level 19'
+      },
+      {
+        name: 'Rainfall Glitch',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 15 kills on operators affectred by your tactical',
+        requirement: 'Weapon level 29'
+      },
+      {
+        name: 'Gilded',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills without the enemy damaging you',
+        requirement: 'Complete all base camo challenges'
+      },
+      {
+        name: 'Forged',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills shortly after ADS',
+        requirement: 'Complete 6 gilded camo challenges with assult rifles'
+      },
+      {
+        name: 'Priceless',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills being out of the enemy\'s line of sight',
+        requirement: 'Complete 36 forged camo challenges'
+      }
+    ]
+  },
+  {
+    name: 'WSP-9',
+    category: 'Submachine gun',
+    camofluages: [
+      {
+        name: 'Golden Poison',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 50 kills',
+        requirement: 'Weapon level 2'
+      },
+      {
+        name: 'Shattered Rock',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 25 hipfire kills',
+        requirement: 'Weapon level 12'
+      },
+      {
+        name: 'Orange',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills while croucing or sliding',
+        requirement: 'Weapon level 19'
+      },
+      {
+        name: 'Orange Sluphur',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 15 kills on operators affectred by your tactical',
+        requirement: 'Weapon level 28'
+      },
+      {
+        name: 'Gilded',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills without the enemy damaging you',
+        requirement: 'Complete all base camo challenges'
+      },
+      {
+        name: 'Forged',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills while using a magnification scope',
+        requirement: 'Complete 6 gilded camo challenges with assult rifles'
+      },
+      {
+        name: 'Priceless',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 suppressed headshots',
+        requirement: 'Complete 36 forged camo challenges'
+      }
+    ]
+  },
+  {
+    name: 'RIVAL-9',
+    category: 'Submachine gun',
+    camofluages: [
+      {
+        name: 'Rock Stone',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 50 kills',
+        requirement: 'Weapon level 2'
+      },
+      {
+        name: 'Rock Glow',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 25 hipfire kills',
+        requirement: 'Weapon level 9'
+      },
+      {
+        name: 'Deluge Glitch',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills while croucing or sliding',
+        requirement: 'Weapon level 14'
+      },
+      {
+        name: 'Bacterial Decay',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 15 kills on operators affectred by your tactical',
+        requirement: 'Weapon level 20'
+      },
+      {
+        name: 'Gilded',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills without the enemy damaging you',
+        requirement: 'Complete all base camo challenges'
+      },
+      {
+        name: 'Forged',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills shortly after ADS',
+        requirement: 'Complete 6 gilded camo challenges with assult rifles'
+      },
+      {
+        name: 'Priceless',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 suppressed double kills',
+        requirement: 'Complete 36 forged camo challenges'
+      }
+    ]
+  },
+  {
+    name: 'STRIKER-9',
+    category: 'Submachine gun',
+    camofluages: [
+      {
+        name: 'Marbled',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 50 kills',
+        requirement: 'Weapon level 2'
+      },
+      {
+        name: 'Topo Hardpan',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 25 hipfire kills',
+        requirement: 'Weapon level 11'
+      },
+      {
+        name: 'Yellow Sands',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills while croucing or sliding',
+        requirement: 'Weapon level 17'
+      },
+      {
+        name: 'Desolate Ops',
+        set: '',
+        type: 'BASE',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 15 kills on operators affectred by your tactical',
+        requirement: 'Weapon level 25'
+      },
+      {
+        name: 'Gilded',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 10 kills without the enemy damaging you',
+        requirement: 'Complete all base camo challenges'
+      },
+      {
+        name: 'Forged',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 15 longshot kills',
+        requirement: 'Complete 6 gilded camo challenges with assult rifles'
+      },
+      {
+        name: 'Priceless',
+        set: '',
+        type: 'COMPLETIONIST',
+        gameMode: 'MULTIPLAYER',
+        challenge: 'Get 15 headshots while moving',
         requirement: 'Complete 36 forged camo challenges'
       }
     ]
